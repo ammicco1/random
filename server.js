5f8fa261389335ccde1e7891d6af0ac17ab5f4ad
ammicco <ngrecchi1@gmail.com>
2023-07-16 21:21:39 +0200

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const server = express();
+
+const port = process.argv[2] || 3000;
+
+server.listen(port, function(){
+    console.debug(`random listen on port ${port}`);
+});
+
+server.get("/", function(req, res){
+    res.sendFile(`${__dirname}/views/index.html`);
+});
+
+server.get("/string", function(req, res){
+    const len = req.query.len || 32;
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?^+*#@.,:;-_$%&=";
+    const charsLength = chars.length;
+    
+    let result = "";
+    let counter = 0;
+    
+    while(counter < len){
+        result += chars.charAt(Math.floor(Math.random() * charsLength));
+        counter++;
+    }
+    
+    res.send(result);
+});
+
+server.get("/int", function(req, res){
+    const max = req.query.max || 10000;
+    let num = Math.floor(Math.random() * max).toString();
+
+    res.send(num);
+});
