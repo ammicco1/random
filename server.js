5212545fc233919691bef8058f2e01d3cfeb13dc
ammicco <ngrecchi1@gmail.com>
2023-07-18 19:04:08 +0200

    add random json

--- server.js
+++ server.js
@@ -43,3 +43,66 @@ server.get("/int", function(req, res){
 
     res.send(num);
 });
+
+server.get("/json", function(req, res){
+    let max = Math.floor(Math.random() * 15);
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const charsLength = chars.length;
+    
+    let str, str2;
+    let counter;
+    let choise;
+
+    let json = {}; 
+    
+    while(max == 0){
+        max = Math.floor(Math.random() * 15);
+    }
+
+    for(i = 0; i < max; i++){
+        str = "";
+        counter = 0;
+
+        while(counter < 6){
+            str += chars.charAt(Math.floor(Math.random() * charsLength));
+            counter++;
+        }
+
+        choise = Math.floor(Math.random() * 10);
+
+        switch(choise % 3){
+            case 0: json[str] = Math.floor(Math.random() * 1000); break;
+            case 1: str2 = "";
+                counter = 0;
+
+                while(counter < 10){
+                    str2 += chars.charAt(Math.floor(Math.random() * charsLength));
+                    counter++;
+                }
+
+                json[str] = str2; break;
+            case 2: let array = [];
+                let alen = Math.floor(Math.random() * 10);
+
+                for(i = 0; i < alen; i++){
+                    if(Math.floor(Math.random() * 10) % 2 == 0){
+                        array.push(Math.floor(Math.random() * 1000));
+                    }else{
+                        var str3 = "";
+                        let c = 0;
+
+                        while(c < 8){
+                            str3 += chars.charAt(Math.floor(Math.random() * charsLength));
+                            c++;
+                        }
+                    }
+
+                    array.push(str3);
+                }
+
+                json[str] = array; break;
+        }
+    }
+
+    res.send(json);
+});
